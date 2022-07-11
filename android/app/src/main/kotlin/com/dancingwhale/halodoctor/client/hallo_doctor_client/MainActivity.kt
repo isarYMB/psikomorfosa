02cf5744f349c35a58e24f0771cb921e6bd161d2
amsyary <amsyary.skenza313@gmail.com>
2022-07-11 13:48:35 +0800

    migration to new project to fix package change name

---
+++ android/app/src/main/kotlin/com/dancingwhale/halodoctor/client/hallo_doctor_client/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.dancingwhale.halodoctor.client.hallo_doctor_client
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+class MainActivity: FlutterFragmentActivity() {
+}
